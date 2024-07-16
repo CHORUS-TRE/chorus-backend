22e7f123b15ae07bab4f19380f505f0d68a1c034
Sami <sami.perrin@chuv.ch>
2024-07-16 18:07:23 +0200

    re

--- docker/build-stage1.sh
+++ docker/build-stage1.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker build --pull -f dockerfiles/stage1.dockerfile -t registry.dip-dev.thehip.app/chorus-tage1 ..
+docker build --pull -f dockerfiles/stage1.dockerfile -t registry.dip-dev.thehip.app/chorus-stage1 ..
 docker push registry.dip-dev.thehip.app/chorus-stage1
