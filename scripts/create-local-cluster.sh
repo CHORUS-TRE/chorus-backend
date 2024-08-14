2681f31c4077948d3aaa81ac277ceabc520fbf30
Sami <sami.perrin@chuv.ch>
2024-08-14 10:58:12 +0200

    clean

--- scripts/create-local-cluster.sh
+++ scripts/create-local-cluster.sh
@@ -36,13 +36,15 @@ kubectl apply -f internal/client/helm/chart/workbench-crd.yaml
 
 ### TODO clean
 
-# pull local image controller 
-# TODO get it locally first
+# pull local image controller
+docker login registry.build.chorus-tre.ch 
+docker pull registry.build.chorus-tre.ch/backend/workbench-controller:latest
+docker tag registry.build.chorus-tre.ch/backend/workbench-controller:latest controller:latest
+docker pull registry.build.chorus-tre.ch/xpra-server:latest
 kind load docker-image controller:latest --name chorus
+kind load docker-image registry.build.chorus-tre.ch/xpra-server:latest --name chorus
 
-# TODO launch operator
-cd /home/localadmin/chuv/chorus/workbench-operator
-kubectl apply -k config/manager
+kubectl apply -k internal/client/helm/chart/manager
 
 cd "$DIR"
 kubectl apply -f internal/client/helm/chart/roles.yaml
