d35d5473a7cdecd63aa13c9fa624216756044438
Sami <sami.perrin@chuv.ch>
2024-08-14 09:46:13 +0200

    pregenerated certs

--- scripts/create-local-cluster.sh
+++ scripts/create-local-cluster.sh
@@ -28,8 +28,21 @@ done
 if [ $exists -eq 1 ]; then
     echo "Cluster chorus already exist, skipping create..."
 else
-    kind create cluster --name chorus
+    kind create cluster --name chorus --config configs/dev/files/kind-config.yaml
 fi
 
 # create workbench CRD
 kubectl apply -f internal/client/helm/chart/workbench-crd.yaml
+
+### TODO clean
+
+# pull local image controller 
+# TODO get it locally first
+kind load docker-image controller:latest --name chorus
+
+# TODO launch operator
+cd /home/localadmin/chuv/chorus/workbench-operator
+kubectl apply -k config/manager
+
+cd "$DIR"
+kubectl apply -f internal/client/helm/chart/roles.yaml
