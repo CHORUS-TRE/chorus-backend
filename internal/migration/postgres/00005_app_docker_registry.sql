bd1382299a4ade7ebff2e7cee8bea2ec492113da
Sami <sami.perrin@chuv.ch>
2024-11-13 15:53:28 +0100

    fix-typo

--- internal/migration/postgres/00005_app_docker_registry.sql
+++ internal/migration/postgres/00005_app_docker_registry.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
 
 -- +migrate StatementBegin
-ALTER TABLE apps ADD COLUMN dockerimagerregistry TEXT NOT NULL DEFAULT '';
+ALTER TABLE apps ADD COLUMN dockerimageregistry TEXT NOT NULL DEFAULT '';
 -- +migrate StatementEnd
