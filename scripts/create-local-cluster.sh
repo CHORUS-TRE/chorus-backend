b234fc0947e38c163533c904e0f53d2c695e3ff9
Sami <sami.perrin@chuv.ch>
2025-05-15 09:50:38 +0200

    bump versions in create local cluster script

--- scripts/create-local-cluster.sh
+++ scripts/create-local-cluster.sh
@@ -46,10 +46,10 @@ echo "Pulling dependencies"
 echo "Login to harbor.dev.chorus-tre.ch"
 # read -s -p "Password of robot\$chorus-dev: " pw
 docker login harbor.dev.chorus-tre.ch -u "$user" -p "$pw"
-docker pull --platform=linux/amd64 harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.9
+docker pull --platform=linux/amd64 harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.16
 docker tag harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.9 controller:latest
-docker pull --platform=linux/amd64 harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-2
-docker tag harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-2 harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-2
+docker pull --platform=linux/amd64 harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-4
+docker tag harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-4 harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-4
 
 if [ $exists -eq 1 ]; then
     echo "Cluster chorus already exist, skipping create..."
@@ -59,8 +59,8 @@ else
     sleep 10
 fi
 
-kind load docker-image harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.9 --name chorus
-kind load docker-image harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-2 --name chorus
+kind load docker-image harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.16 --name chorus
+kind load docker-image harbor.dev.chorus-tre.ch/apps/xpra-server:6.2.3-4 --name chorus
 
 kubectl apply -f configs/dev/files/deploy-ingress-nginx.yaml
 
@@ -75,7 +75,7 @@ cd workbench-operator
 # kubectl apply -f tmp-workbench-crd.yaml
 # rm tmp-workbench-crd.yaml
 echo "" > config/prometheus/monitor.yaml
-export IMG="harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.9"
+export IMG="harbor.dev.chorus-tre.ch/chorus/workbench-operator:0.3.16"
 make build-installer
 kubectl apply -f dist/install.yaml
 cd ..
@@ -123,7 +123,7 @@ cat <<EOF >configs/dev/files/kind.yaml
 clients:
   k8s_client:
     is_watcher: true
-    server_version: "6.2.3-2"
+    server_version: "6.2.3-4"
     ca: |
 $ca_ident
     token: $token
