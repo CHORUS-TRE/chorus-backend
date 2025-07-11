d8dbb6ba4fc60df452f952317788344ffb207e64
Jérémie Frei <jeremie.frei@chuv.ch>
2025-07-11 11:18:30 +0200

    Add constraint to have unique main workspace per user

--- internal/migration/postgres/00012_add_workspace_ismainworkspace_flag.sql
+++ internal/migration/postgres/00012_add_workspace_ismainworkspace_flag.sql
@@ -2,4 +2,8 @@
 
 -- +migrate StatementBegin
 ALTER TABLE workspaces ADD COLUMN ismain BOOLEAN NOT NULL DEFAULT FALSE;
+
+CREATE UNIQUE INDEX unique_main_workspace_per_user
+ON workspaces(userid)
+WHERE deletedat IS NULL AND ismain = true; 
 -- +migrate StatementEnd
