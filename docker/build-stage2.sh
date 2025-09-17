2f7c0c3b8b22a77e9974ecedc11826064c08d836
Sami <sami.perrin@chuv.ch>
2025-09-17 15:58:41 +0200

    pass as secret

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-docker build --pull -f dockerfiles/stage2.dockerfile -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
+if [ -n "$GIT_USERNAME" -a -n "$GIT_PASSWORD" ]; then
+    docker build --pull -f dockerfiles/stage2.dockerfile --secret id=GIT_USERNAME,env=GIT_USERNAME --secret id=GIT_PASSWORD,env=GIT_PASSWORD -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
+else
+    docker build --pull -f dockerfiles/stage2.dockerfile -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
+fi
+
 docker push harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG}
 if [[ "$BRANCH_NAME" == "master" ]]; then
     docker tag harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} harbor.build.chorus-tre.ch/chorus/backend:master
--- docker/dockerfiles/stage2.dockerfile
+++ docker/dockerfiles/stage2.dockerfile
@@ -8,6 +8,17 @@ COPY . /chorus
 ENV GOCACHE="/chorus/.cache/go-build"
 ENV GOMODCACHE="/chorus/.cache/go-mod"
 
+RUN --mount=type=secret,id=GIT_USERNAME \
+    --mount=type=secret,id=GIT_PASSWORD \
+    u="$(cat /run/secrets/GIT_USERNAME)"; \
+    p="$(cat /run/secrets/GIT_PASSWORD)"; \
+    [ -n "$u" ] && [ -n "$p" ] && \
+    GOPRIVATE=github.com/CHORUS-TRE/* \
+    GIT_CONFIG_COUNT=1 \
+    GIT_CONFIG_KEY_0=url."https://${u}:${p}@github.com/".insteadof \
+    GIT_CONFIG_VALUE_0=https://github.com/ \
+    go mod download
+
 RUN --mount=type=cache,target="/chorus/.cache/go-build" --mount=type=cache,target="/chorus/.cache/go-mod" cd /chorus/cmd/chorus && \
     go build -trimpath -ldflags "$LD_FLAGS" -o /chorus/bin/chorus
 
