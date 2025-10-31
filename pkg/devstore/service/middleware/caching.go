b11a994aebbf93b343f162be83833c99ab782e91
Jérémie Frei <jeremie.frei@chuv.ch>
2025-10-31 11:53:05 +0100

    Fix service caching middleware

--- pkg/devstore/service/middleware/caching.go
+++ pkg/devstore/service/middleware/caching.go
@@ -32,21 +32,30 @@ type Caching struct {
 	next  service.Devstorer
 }
 
-func (s *Caching) ListEntries(ctx context.Context, tenantID uint64, scope model.DevStoreScope, scopeID uint64) ([]*model.DevstoreEntry, error) {
-	entry := s.cache.NewEntry(cache.WithUint64(scopeID), cache.WithUint64(scopeID))
-	entries := []*model.DevstoreEntry{}
+func (s *Caching) ListEntries(ctx context.Context, tenantID uint64, scope model.DevStoreScope, scopeID uint64) (reply []*model.DevstoreEntry, err error) {
+	entry := s.cache.NewEntry(cache.WithUint64(tenantID), cache.WithString(string(scope)), cache.WithUint64(scopeID))
+	reply = []*model.DevstoreEntry{}
 
-	if ok := entry.Get(ctx, &entries); !ok {
-		entries, err := s.next.ListEntries(ctx, tenantID, scope, scopeID)
+	if ok := entry.Get(ctx, &reply); !ok {
+		reply, err = s.next.ListEntries(ctx, tenantID, scope, scopeID)
 		if err == nil {
-			entry.Set(ctx, defaultCacheExpiration, entries)
+			entry.Set(ctx, defaultCacheExpiration, reply)
 		}
 	}
-	return entries, nil
+	return
 }
 
-func (s *Caching) GetEntry(ctx context.Context, tenantID uint64, scope model.DevStoreScope, scopeID uint64, key string) (*model.DevstoreEntry, error) {
-	return s.next.GetEntry(ctx, tenantID, scope, scopeID, key)
+func (s *Caching) GetEntry(ctx context.Context, tenantID uint64, scope model.DevStoreScope, scopeID uint64, key string) (reply *model.DevstoreEntry, err error) {
+	entry := s.cache.NewEntry(cache.WithUint64(tenantID), cache.WithString(string(scope)), cache.WithUint64(scopeID), cache.WithString(key))
+	reply = &model.DevstoreEntry{}
+
+	if ok := entry.Get(ctx, &reply); !ok {
+		reply, err = s.next.GetEntry(ctx, tenantID, scope, scopeID, key)
+		if err == nil {
+			entry.Set(ctx, shortCacheExpiration, reply)
+		}
+	}
+	return
 }
 
 func (s *Caching) PutEntry(ctx context.Context, tenantID uint64, scope model.DevStoreScope, scopeID uint64, key string, value string) (*model.DevstoreEntry, error) {
