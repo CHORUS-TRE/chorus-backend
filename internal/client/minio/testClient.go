3a964d02833c8638bbde8168136519de6bc6bb11
Jérémie Frei <jeremie.frei@chuv.ch>
2025-11-04 16:26:43 +0100

    update minio test client to return not implemented errors

--- internal/client/minio/testClient.go
+++ internal/client/minio/testClient.go
@@ -1,6 +1,10 @@
 package minio
 
-import workspace_model "github.com/CHORUS-TRE/chorus-backend/pkg/workspace/model"
+import (
+	"fmt"
+
+	workspace_model "github.com/CHORUS-TRE/chorus-backend/pkg/workspace/model"
+)
 
 var _ MinioClienter = &testClient{}
 
@@ -11,21 +15,21 @@ func NewTestClient() *testClient {
 }
 
 func (c *testClient) StatWorkspaceObject(workspaceID uint64, path string) (*workspace_model.WorkspaceFile, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Minio Test client not yet implemented")
 }
 
 func (c *testClient) ListWorkspaceObjects(workspaceID uint64, path string) ([]*workspace_model.WorkspaceFile, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Minio Test client not yet implemented")
 }
 
 func (c *testClient) GetWorkspaceObject(workspaceID uint64, path string) (*workspace_model.WorkspaceFile, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Minio Test client not yet implemented")
 }
 
 func (c *testClient) PutWorkspaceObject(workspaceID uint64, file *workspace_model.WorkspaceFile) (*workspace_model.WorkspaceFile, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Minio Test client not yet implemented")
 }
 
 func (c *testClient) DeleteWorkspaceObject(workspaceID uint64, path string) error {
-	return nil
+	return fmt.Errorf("Minio Test client not yet implemented")
 }
