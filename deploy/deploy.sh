f31964c0dbf0aae376779947043fc45c1bc04bf1
Jonathan Haab <jonathan.haab@chuv.ch>
2025-11-05 16:04:57 +0100

    disable metrics and servicemonitor for postgres

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,14 @@ set -e
 
 helm version
 helm dependency update ./backend
-helm template chorus-jenkins-backend ./backend --namespace "$env" --values ./backend/values.yaml --values ../configs/$env/values.yaml --set-string "image.tag=${IMAGE_TAG}" > chorus-jenkins-backend.yaml
+helm template chorus-jenkins-backend ./backend \
+  --namespace "$env" \
+  --values ./backend/values.yaml \
+  --values ../configs/$env/values.yaml \
+  --set-string "image.tag=${IMAGE_TAG}" \
+  --set postgresql.metrics.enabled=false \
+  --set postgresql.metrics.serviceMonitor.enabled=false \
+  > chorus-jenkins-backend.yaml
 
 echo ""
 echo "deploying..."
