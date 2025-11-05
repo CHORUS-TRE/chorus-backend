bcbd7e541c4bd5d620a7047ab6a90b635f0ba162
Jonathan Haab <jonathan.haab@chuv.ch>
2025-11-05 16:02:08 +0100

    load the default values as well as the overriding ones

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 helm version
 helm dependency update ./backend
-helm template chorus-jenkins-backend ./backend --namespace "$env" --values ../configs/$env/values.yaml --set-string "image.tag=${IMAGE_TAG}" > chorus-jenkins-backend.yaml
+helm template chorus-jenkins-backend ./backend --namespace "$env" --values ./backend/values.yaml --values ../configs/$env/values.yaml --set-string "image.tag=${IMAGE_TAG}" > chorus-jenkins-backend.yaml
 
 echo ""
 echo "deploying..."
