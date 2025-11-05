3caf399e9d1d683053323741f363f85c9d9b97f6
Jonathan Haab <jonathan.haab@chuv.ch>
2025-11-05 14:40:06 +0100

    refactor script used by jenkins

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,19 +2,10 @@
 
 set -e
 
-mkdir -p ./backend/files
-rm -rf ./backend/files/*
-cp -r ../configs/$env/* ./backend/files/
-
 helm version
-CONFIG_AES_PASSPHRASE_VAR_NAME="CONFIG_AES_PASSPHRASE_$env"
-helm template --namespace "$env" --values ./backend/files/values.yaml --set-string "aesPassphrase=${!CONFIG_AES_PASSPHRASE_VAR_NAME}" --set-string "image=harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG}" ./backend
+helm dependency update ./backend
+helm template --namespace "$env" --values ../configs/$env/values.yaml --set-string "image.tag=${IMAGE_TAG}" ./backend
 
-echo "CONFIG_AES_PASSPHRASE"
-echo $CONFIG_AES_PASSPHRASE_VAR_NAME
-echo ${!CONFIG_AES_PASSPHRASE_VAR_NAME}
-
-echo ""
-echo "deploying..."
-helm upgrade --install --create-namespace --namespace "$env" --values ./backend/files/values.yaml --set-string "aesPassphrase=${!CONFIG_AES_PASSPHRASE_VAR_NAME}" --set-string "version=$DEPLOY_VERSION" --set-string "image=harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG}" "${RELEASE_NAME}" ./backend
+echo "\ndeploying..."
+helm upgrade --install --create-namespace --namespace "$env" --values ./backend/files/values.yaml --set-string "version=$DEPLOY_VERSION" --set-string "image.tag=${IMAGE_TAG}" "${RELEASE_NAME}" ./backend
 echo "done"
