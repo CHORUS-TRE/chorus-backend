0cd5bde50651f759875e994aab25664149ee71db
Jonathan Haab <jonathan.haab@chuv.ch>
2025-11-05 15:06:03 +0100

    fix echo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ helm version
 helm dependency update ./backend
 helm template chorus-jenkins-backend ./backend --namespace "$env" --values ../configs/$env/values.yaml --set-string "image.tag=${IMAGE_TAG}" > chorus-jenkins-backend.yaml
 
-echo "\ndeploying..."
+echo ""
+echo "deploying..."
 kubectl apply -f chorus-jenkins-backend.yaml --namespace "$env"
 echo "done"
