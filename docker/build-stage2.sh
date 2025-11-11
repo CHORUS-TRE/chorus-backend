3ae454ce3ba2200d6e23758eb437633e7c9d9d62
Jonathan Haab <jonathan.haab@chuv.ch>
2025-11-11 15:00:53 +0100

    revert, append empty line

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -n "$GIT_USERNAME" -a -n "$GIT_PASSWORD" ]; then
-    docker build --pull -f dockerfiles/backend.dockerfile --secret id=GIT_USERNAME,env=GIT_USERNAME --secret id=GIT_PASSWORD,env=GIT_PASSWORD -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
+    docker build --pull -f dockerfiles/stage2.dockerfile --secret id=GIT_USERNAME,env=GIT_USERNAME --secret id=GIT_PASSWORD,env=GIT_PASSWORD -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
 else
-    docker build --pull -f dockerfiles/backend.dockerfile -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
+    docker build --pull -f dockerfiles/stage2.dockerfile -t harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG} ..
 fi
 
 docker push harbor.build.chorus-tre.ch/chorus/backend:${IMAGE_TAG}
