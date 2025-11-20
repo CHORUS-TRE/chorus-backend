6b6f2cdac6f389b1fd3482b33e210c2d6bd613e7
Jérémie Frei <jeremie.frei@chuv.ch>
2025-11-20 10:15:48 +0100

    implement full filesystem abstraction on minio store

--- internal/client/minio/minio-file-store.go
+++ internal/client/minio/minio-file-store.go
@@ -10,13 +10,32 @@ import (
 	"github.com/CHORUS-TRE/chorus-backend/internal/logger"
 )
 
+// The MinioFileStore interface abstracts UNIX-like file operations which can be performed on a MinIO object storage.
+// The directories are represented as objects with keys ending in a '/' character.
 type MinioFileStore interface {
-	GetFileMetadata(ctx context.Context, filePath string) (*model.File, error)
-	GetFile(ctx context.Context, filePath string) (*model.File, error)
-	ListFiles(ctx context.Context, filePath string) ([]*model.File, error)
+	// Get file metadata at the specified path without downloading the content.
+	StatFile(ctx context.Context, path string) (*model.File, error)
+
+	// Get the file at the specified path, including its content.
+	GetFile(ctx context.Context, path string) (*model.File, error)
+
+	// List files and directories at the specified path.
+	ListFiles(ctx context.Context, path string) ([]*model.File, error)
+
+	// Create a new file at the specified path.
 	CreateFile(ctx context.Context, file *model.File) (*model.File, error)
-	UpdateFile(ctx context.Context, oldPath string, file *model.File) (*model.File, error)
-	DeleteFile(ctx context.Context, filePath string) error
+
+	// Create a new directory at the specified path.
+	CreateDirectory(ctx context.Context, file *model.File) (*model.File, error)
+
+	// Move a file from oldPath to newPath.
+	MoveFile(ctx context.Context, oldPath string, newPath string) (*model.File, error)
+
+	// Delete a file at the specified path.
+	DeleteFile(ctx context.Context, path string) error
+
+	// Delete a directory and all its contents recursively.
+	DeleteDirectory(ctx context.Context, path string) error
 }
 
 var _ MinioFileStore = &MinioFileStorage{}
@@ -31,22 +50,22 @@ func NewMinioFileStorage(client miniorawclient.MinioClienter) (*MinioFileStorage
 	}, nil
 }
 
-func (s *MinioFileStorage) GetFileMetadata(ctx context.Context, objectKey string) (*model.File, error) {
-	objectInfo, err := s.minioClient.StatObject(objectKey)
+func (s *MinioFileStorage) StatFile(ctx context.Context, path string) (*model.File, error) {
+	objectInfo, err := s.minioClient.StatObject(path)
 	if err != nil {
-		return nil, fmt.Errorf("unable to stat object %s: %w", objectKey, err)
+		return nil, fmt.Errorf("unable to stat file at %s: %w", path, err)
 	}
 
 	file := model.MinioObjectInfoToFile(objectInfo)
 
-	logger.TechLog.Info(ctx, fmt.Sprintf("retrieved metadata for %s", objectKey))
+	logger.TechLog.Info(ctx, fmt.Sprintf("Fetched metadata for %s", path))
 	return file, nil
 }
 
 func (s *MinioFileStorage) GetFile(ctx context.Context, path string) (*model.File, error) {
 	object, err := s.minioClient.GetObject(path)
 	if err != nil {
-		return nil, fmt.Errorf("unable to get object %s: %w", path, err)
+		return nil, fmt.Errorf("unable to get file at %s: %w", path, err)
 	}
 
 	file := model.MinioObjectToFile(object)
@@ -58,7 +77,7 @@ func (s *MinioFileStorage) GetFile(ctx context.Context, path string) (*model.Fil
 func (s *MinioFileStorage) ListFiles(ctx context.Context, path string) ([]*model.File, error) {
 	objects, err := s.minioClient.ListObjects(path, false)
 	if err != nil {
-		return nil, fmt.Errorf("unable to list objects with prefix %s: %w", path, err)
+		return nil, fmt.Errorf("unable to list files at path %s: %w", path, err)
 	}
 
 	var files []*model.File
@@ -72,81 +91,126 @@ func (s *MinioFileStorage) ListFiles(ctx context.Context, path string) ([]*model
 }
 
 func (s *MinioFileStorage) CreateFile(ctx context.Context, file *model.File) (*model.File, error) {
-	// Check if a directory or file does not already exist at path
-	object, err := s.minioClient.ListObjects(strings.TrimSuffix(file.Path, "/"), false)
-	if err != nil {
-		return nil, fmt.Errorf("unable to check existing objects at %s: %w", file.Path, err)
-	}
-	if len(object) > 0 {
-		return nil, fmt.Errorf("object already exists at %s", file.Path)
+	if file.IsDirectory {
+		return nil, fmt.Errorf("use CreateDirectory to create directories")
 	}
 
-	// Upload
-	_, err = s.minioClient.PutObject(file.Path, model.FileToMinioObject(file))
-	if err != nil {
-		return nil, fmt.Errorf("unable to put object at %s: %w", file.Path, err)
+	path := strings.TrimSuffix(file.Path, "/")
+
+	// Check for existing file
+	existingObject, err := s.minioClient.StatObject(path)
+	if existingObject != nil {
+		return nil, fmt.Errorf("a file already exists at %s", path)
 	}
 
-	// Verify upload
-	objectInfo, err := s.minioClient.StatObject(file.Path)
-	if err != nil {
-		return nil, fmt.Errorf("unable to verify created object: %w", err)
+	// Check for conflicting directory
+	dirPath := path + "/"
+	object, err := s.minioClient.StatObject(dirPath)
+	if object != nil {
+		return nil, fmt.Errorf("a directory with conflicting name exists at %s", dirPath)
 	}
 
-	logger.TechLog.Info(ctx, fmt.Sprintf("Created %s", file.Path))
+	// Upload file
+	_, err = s.minioClient.PutObject(path, model.FileToMinioObject(file))
+	if err != nil {
+		return nil, fmt.Errorf("unable to upload file at %s: %w", path, err)
+	}
+
+	// Verify file upload
+	objectInfo, err := s.minioClient.StatObject(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to verify created file at %s: %w", path, err)
+	}
+
+	logger.TechLog.Info(ctx, fmt.Sprintf("Created %s", path))
 
 	createdFile := model.MinioObjectInfoToFile(objectInfo)
 
 	return createdFile, nil
 }
 
-func (s *MinioFileStorage) UpdateFile(ctx context.Context, oldPath string, file *model.File) (*model.File, error) {
-	// Check if old file exists
-	_, err := s.minioClient.StatObject(oldPath)
-	if err != nil {
-		return nil, fmt.Errorf("object at %s does not exist: %w", oldPath, err)
+func (s *MinioFileStorage) CreateDirectory(ctx context.Context, file *model.File) (*model.File, error) {
+	if file.IsDirectory == false {
+		return nil, fmt.Errorf("use CreateFile to create files")
 	}
 
-	// Upload new file
-	_, err = s.minioClient.PutObject(file.Path, model.FileToMinioObject(file))
-	if err != nil {
-		return nil, fmt.Errorf("unable to put object at %s: %w", file.Path, err)
+	dirPath := strings.TrimSuffix(file.Path, "/") + "/"
+
+	// Check for existing directory
+	existingObject, err := s.minioClient.StatObject(dirPath)
+	if existingObject != nil {
+		return nil, fmt.Errorf("a directory already exists at %s", dirPath)
 	}
 
-	// Delete old file
-	err = s.minioClient.DeleteObject(oldPath)
-	if err != nil {
-		return nil, fmt.Errorf("unable to delete old object at %s: %w", oldPath, err)
+	// Check for conflicting file
+	object, err := s.minioClient.StatObject(strings.TrimSuffix(dirPath, "/"))
+	if object != nil {
+		return nil, fmt.Errorf("a file with conflicting name exists at %s", strings.TrimSuffix(dirPath, "/"))
 	}
 
-	// Verify upload
-	objectInfo, err := s.minioClient.StatObject(file.Path)
+	// Create directory as an empty object with trailing '/'
+	_, err = s.minioClient.PutObject(dirPath, &miniorawclient.MinioObject{
+		MinioObjectInfo: miniorawclient.MinioObjectInfo{
+			Key: dirPath,
+		},
+		Content: []byte{},
+	})
 	if err != nil {
-		return nil, fmt.Errorf("unable to verify updated object: %w", err)
+		return nil, fmt.Errorf("unable to create directory at %s: %w", dirPath, err)
 	}
 
-	logger.TechLog.Info(ctx, fmt.Sprintf("Updated %s to %s", oldPath, file.Path))
+	// Verify directory creation
+	objectInfo, err := s.minioClient.StatObject(dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to verify created directory at %s: %w", dirPath, err)
+	}
 
-	updatedFile := model.MinioObjectInfoToFile(objectInfo)
+	logger.TechLog.Info(ctx, fmt.Sprintf("Created directory %s", dirPath))
 
-	return updatedFile, nil
+	createdDir := model.MinioObjectInfoToFile(objectInfo)
+
+	return createdDir, nil
+}
+
+func (s *MinioFileStorage) MoveFile(ctx context.Context, oldPath string, newPath string) (*model.File, error) {
+	err := s.minioClient.MoveObject(oldPath, newPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to move file from %s to %s: %w", oldPath, newPath, err)
+	}
+
+	// Verify moved file
+	objectInfo, err := s.minioClient.StatObject(newPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to verify moved file at %s: %w", newPath, err)
+	}
+
+	logger.TechLog.Info(ctx, fmt.Sprintf("Moved file from %s to %s", oldPath, newPath))
+
+	movedFile := model.MinioObjectInfoToFile(objectInfo)
+
+	return movedFile, nil
 }
 
 func (s *MinioFileStorage) DeleteFile(ctx context.Context, path string) error {
-	if path[len(path)-1] == '/' {
-		return s.deleteDirectory(ctx, path)
+	if strings.HasSuffix(path, "/") {
+		return fmt.Errorf("use DeleteDirectory to delete directories")
 	}
 
 	err := s.minioClient.DeleteObject(path)
 	if err != nil {
-		return fmt.Errorf("unable to delete object at %s: %w", path, err)
+		return fmt.Errorf("unable to delete file at %s: %w", path, err)
 	}
 
-	logger.TechLog.Info(ctx, fmt.Sprintf("Deleted %s", path))
+	logger.TechLog.Info(ctx, fmt.Sprintf("Deleted file at %s", path))
 	return nil
 }
 
-func (s *MinioFileStorage) deleteDirectory(ctx context.Context, path string) error {
+func (s *MinioFileStorage) DeleteDirectory(ctx context.Context, path string) error {
+	if !strings.HasSuffix(path, "/") {
+		return fmt.Errorf("use DeleteFile to delete files")
+	}
+
+	// List all objects with the given prefix, recursively
 	objects, err := s.minioClient.ListObjects(path, true)
 	if err != nil {
 		return fmt.Errorf("unable to list objects with prefix %s: %w", path, err)
