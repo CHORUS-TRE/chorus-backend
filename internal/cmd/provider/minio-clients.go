8f56f7397f13a477b7b690fb99bbd2d77d6b5214
Sami <sami.perrin@chuv.ch>
2025-12-16 17:09:01 +0100

    rm

--- internal/cmd/provider/minio-clients.go
+++
@@ -1,5 +0,0 @@
-package provider
-
-// This file is deprecated. MinIO clients are now created directly in the workspace-file.go provider
-// based on the BlockStore configuration instead of being managed separately.
-// The MinIO configuration is now part of the BlockStoreMinioConfig in the config.go file.
