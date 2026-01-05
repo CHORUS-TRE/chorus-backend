f08908f00f3b8eea747f6ffbc22aed34eaf1ad8c
Sami <sami.perrin@chuv.ch>
2026-01-05 16:47:01 +0100

    bump

--- tests/acceptance/info/info_test.go
+++ tests/acceptance/info/info_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/CHORUS-TRE/chorus-backend/internal/cmd/provider"
 	"github.com/CHORUS-TRE/chorus-backend/tests/helpers"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
