5a9aab90d192074bb6355735a86ee4741f5644a1
Jérémie Frei <jeremie.frei@chuv.ch>
2026-01-08 17:00:17 +0100

    fix: remove K8SState and K8SStatus from AppInstance conversion

--- internal/api/v1/converter/app-instance.go
+++ internal/api/v1/converter/app-instance.go
@@ -59,9 +59,7 @@ func AppInstanceFromBusiness(appInstance *model.AppInstance) (*chorus.AppInstanc
 		WorkspaceId: appInstance.WorkspaceID,
 		WorkbenchId: appInstance.WorkbenchID,
 
-		Status:    appInstance.Status.String(),
-		K8SState:  appInstance.K8sState.String(),
-		K8SStatus: appInstance.K8sStatus.String(),
+		Status: appInstance.Status.String(),
 
 		InitialResolutionWidth:  appInstance.InitialResolutionWidth,
 		InitialResolutionHeight: appInstance.InitialResolutionHeight,
