5d4681949229f44ea2a91cbd8048c7625dad2e41
Jérémie Frei <jeremie.frei@chuv.ch>
2026-01-19 14:38:17 +0100

    feat: add audit migration files and initial migration

---
+++ internal/migration/audit/postgres/00001_initial_migration.sql
@@ -0,0 +1,17 @@
+--- +migrate Up
+
+CREATE SEQUENCE public.audit_seq MINVALUE 1 MAXVALUE 9007199254740991 INCREMENT 1 START 1;
+-- +migrate StatementBegin
+CREATE TABLE public.audit (
+    id BIGINT NOT NULL DEFAULT nextval('public.audit_seq'::REGCLASS),
+    tenantid BIGINT NULL,
+    userid BIGINT NULL,
+    username TEXT NULL,
+    action TEXT NULL,
+    resourcetype TEXT NULL,
+    resourceid BIGINT NULL,
+    correlationid TEXT NULL,
+    details JSONB NULL,
+    createdat TIMESTAMP NOT NULL,
+    CONSTRAINT audit_pkey PRIMARY KEY (id)
+);
--- internal/migration/embed.go
+++ internal/migration/embed.go
@@ -4,3 +4,6 @@ import "embed"
 
 //go:embed postgres/*
 var MigrationEmbed embed.FS
+
+//go:embed audit/postgres/*
+var AuditMigrationEmbed embed.FS
