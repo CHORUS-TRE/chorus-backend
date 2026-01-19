4eb9f88e24468f4649ba3fef5b05f5a6bc9e0655
Jérémie Frei <jeremie.frei@chuv.ch>
2026-01-19 14:37:15 +0100

    refactor: move migrations methods

--- internal/migration/chorus.go
+++ internal/migration/chorus.go
@@ -1,12 +1,7 @@
 package migration
 
 import (
-	"embed"
 	"fmt"
-	"io"
-	"io/fs"
-	"path/filepath"
-	"strings"
 )
 
 const (
@@ -29,51 +24,6 @@ func getMigration(path string) (map[string]string, error) {
 		res[removeFileExtension(file)] = content
 	}
 	return res, nil
-}
-
-func readFile(migrationFS embed.FS, file string) (string, error) {
-	r, err := migrationFS.Open(file)
-	if err != nil {
-		return "", err
-	}
-	defer r.Close()
-
-	contents, err := io.ReadAll(r)
-	if err != nil {
-		return "", err
-	}
-	return string(contents), nil
-}
-
-func filePath(storageType, fileName string) string {
-	return fmt.Sprintf("%s/%s", storageType, fileName)
-}
-
-func removeFileExtension(f string) string {
-	extension := filepath.Ext(f)
-	return strings.TrimSuffix(f, extension)
-}
-
-func listMigrationFiles(migrationFS embed.FS, path string) ([]string, error) {
-	files := []string{}
-
-	err := fs.WalkDir(migrationFS, path, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		info, err := d.Info()
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			files = append(files, info.Name())
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return files, nil
 }
 
 func GetMigration(storageType string) (map[string]string, string, error) {
