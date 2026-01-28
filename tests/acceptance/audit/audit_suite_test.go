7b980f254717819a0ef2539001df19b7c6b29674
Jérémie Frei <jeremie.frei@chuv.ch>
2026-01-28 14:48:10 +0100

    temp audit acceptance tests

---
+++ tests/acceptance/audit/audit_suite_test.go
@@ -0,0 +1,35 @@
+//go:build acceptance
+
+package audit_test
+
+import (
+	"testing"
+
+	"github.com/CHORUS-TRE/chorus-backend/tests/helpers"
+
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2/reporters"
+	. "github.com/onsi/gomega"
+)
+
+func TestAuditService(t *testing.T) {
+	RegisterFailHandler(Fail)
+	junitReporter := reporters.NewJUnitReporter("junit.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "Audit Service Suite", []Reporter{junitReporter})
+}
+
+var _ = AfterSuite(func() {
+	// cleanTables() - defined in audit_test.go
+})
+
+func Then(text string, body func()) bool {
+	return It("then "+text, body)
+}
+
+func ExpectAPIErr(err interface{}) Assertion {
+	return helpers.ExpectAPIError(err)
+}
+
+func Given(text string, body func()) bool {
+	return Context("given "+text, body)
+}
