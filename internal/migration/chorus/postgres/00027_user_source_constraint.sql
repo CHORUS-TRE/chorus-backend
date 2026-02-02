be5d60703cc2b2a9ddc32ec1c3cdf536b91754f5
Sami <5501101+sami-sweng@users.noreply.github.com>
2026-02-02 15:52:02 +0100

    prevent user source update (#171)
    * prevent user source update
    
    * fix migration

---
+++ internal/migration/chorus/postgres/00027_user_source_constraint.sql
@@ -0,0 +1,11 @@
+-- +migrate Up
+
+-- +migrate StatementBegin
+UPDATE users
+    SET (status, username, updatedat) = ('deleted', username || '-' || gen_random_uuid()::text, NOW())
+    WHERE source = '';
+-- +migrate StatementEnd
+
+-- +migrate StatementBegin
+ALTER TABLE users ADD CONSTRAINT users_source_not_empty CHECK (source <> '');
+-- +migrate StatementEnd
