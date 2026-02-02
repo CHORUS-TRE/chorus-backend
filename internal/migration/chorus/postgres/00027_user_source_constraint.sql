9d6c2f9be43b95f73436f886189b4b9539befa99
Sami <sami.perrin@chuv.ch>
2026-02-02 15:36:40 +0100

    fix migration

--- internal/migration/chorus/postgres/00027_user_source_constraint.sql
+++ internal/migration/chorus/postgres/00027_user_source_constraint.sql
@@ -1,11 +1,11 @@
 -- +migrate Up
 
 -- +migrate StatementBegin
-ALTER TABLE users ADD CONSTRAINT users_source_not_empty CHECK (source <> '');
+UPDATE users
+    SET (status, username, updatedat) = ('deleted', username || '-' || gen_random_uuid()::text, NOW())
+    WHERE source = '';
 -- +migrate StatementEnd
 
--- +migrate Down
-
 -- +migrate StatementBegin
-ALTER TABLE users DROP CONSTRAINT IF EXISTS users_source_not_empty;
+ALTER TABLE users ADD CONSTRAINT users_source_not_empty CHECK (source <> '');
 -- +migrate StatementEnd
