1cbe6b254f524e2a81f6879911862f4dde57640a
Sami <sami.perrin@chuv.ch>
2026-02-09 10:07:03 +0100

    fix local ci

--- scripts/run_acceptance_test.sh
+++ scripts/run_acceptance_test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-export TEST_CONFIG_FILE="./../../../configs/ci/values.yaml"
+export TEST_CONFIG_FILE="./../../../configs/ci_local/backend-config.yaml"
 
 go test -p 1 --tags acceptance ./tests/acceptance/...
--- scripts/run_backend_ci.sh
+++ scripts/run_backend_ci.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-yq '.config' ./configs/ci/values.yaml > ./configs/ci/backend-config.yaml
-go run ./cmd/chorus/main.go --config ./configs/ci/backend-config.yaml start | go run ./cmd/logger/main.go
-rm ./configs/ci/backend-config.yaml
+go run ./cmd/chorus/main.go --config ./configs/ci_local/backend-config.yaml start | go run ./cmd/logger/main.go
