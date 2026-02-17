465ad30987a1066d3d1daa350d5cea6a67677f7b
Jérémie Frei <jeremie.frei@chuv.ch>
2026-02-17 10:58:14 +0100

    feat: implement audit controller

---
+++ internal/api/v1/audit_controller.go
@@ -0,0 +1,69 @@
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/CHORUS-TRE/chorus-backend/internal/api/v1/chorus"
+	"github.com/CHORUS-TRE/chorus-backend/internal/api/v1/converter"
+	jwt_model "github.com/CHORUS-TRE/chorus-backend/internal/jwt/model"
+	"github.com/CHORUS-TRE/chorus-backend/internal/logger"
+	"github.com/CHORUS-TRE/chorus-backend/internal/utils/grpc"
+	"github.com/CHORUS-TRE/chorus-backend/pkg/audit/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var _ chorus.AuditServiceServer = (*AuditController)(nil)
+
+// AuditController is the audit service controller handler.
+type AuditController struct {
+	auditService service.AuditReader
+}
+
+func NewAuditController(auditService service.AuditReader) *AuditController {
+	return &AuditController{
+		auditService: auditService,
+	}
+}
+
+func (c *AuditController) ListAuditEntries(ctx context.Context, req *chorus.ListAuditEntriesRequest) (*chorus.ListAuditEntriesReply, error) {
+	if req == nil {
+		logger.TechLog.Error(ctx, "empty request")
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	tenantID, err := jwt_model.ExtractTenantID(ctx)
+	if err != nil {
+		logger.TechLog.Error(ctx, fmt.Sprintf("unable to extract tenant id from context: %v", err))
+		return nil, status.Error(codes.InvalidArgument, "could not extract tenant id from jwt-token")
+	}
+
+	pagination := converter.PaginationToBusiness(req.Pagination)
+
+	filter, err := converter.AuditFilterToBusiness(req.Filter)
+	if err != nil {
+		logger.TechLog.Error(ctx, fmt.Sprintf("invalid audit filter: %v", err))
+		return nil, status.Errorf(codes.InvalidArgument, "invalid filter: %v", err)
+	}
+
+	res, paginationRes, err := c.auditService.List(ctx, tenantID, &pagination, filter)
+	if err != nil {
+		logger.TechLog.Error(ctx, fmt.Sprintf("unable to call 'ListAuditEntries': %v", err.Error()))
+		return nil, status.Errorf(grpc.ErrorCode(err), "unable to call 'ListAuditEntries': %v", err)
+	}
+
+	var entries []*chorus.AuditEntry
+	for _, r := range res {
+		entry, err := converter.AuditEntryFromBusiness(r)
+		if err != nil {
+			logger.TechLog.Error(ctx, fmt.Sprintf("unable to convert audit entry from business model: %v", err))
+			return nil, status.Errorf(codes.Internal, "conversion error: %v", err)
+		}
+		entries = append(entries, entry)
+	}
+
+	paginationResult := converter.PaginationResultFromBusiness(paginationRes)
+
+	return &chorus.ListAuditEntriesReply{Result: &chorus.ListAuditEntriesResult{Entries: entries}, Pagination: paginationResult}, nil
+}
