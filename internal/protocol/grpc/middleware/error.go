ffb112289847c079c194b8350437aa9fe74b077d
Jérémie Frei <jeremie.frei@chuv.ch>
2026-02-25 08:57:55 +0100

    feat: refactor error handling in UnaryErrorInterceptor

--- internal/protocol/grpc/middleware/error.go
+++ internal/protocol/grpc/middleware/error.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"context"
 	"errors"
+	"fmt"
 
-	choruserrors "github.com/CHORUS-TRE/chorus-backend/internal/errors"
+	cerr "github.com/CHORUS-TRE/chorus-backend/internal/errors"
+	"github.com/CHORUS-TRE/chorus-backend/internal/logger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 )
@@ -16,15 +18,18 @@ func UnaryErrorInterceptor(ctx context.Context, req interface{}, info *grpc.Unar
 	}
 
 	// Check if the error is a ChorusError
-	var chorusErr *choruserrors.ChorusError
-	if errors.As(err, &chorusErr) {
-		// TODO: log causedBy error
-		return nil, chorusErr.ToGRPCStatus().Err()
+	var cErr *cerr.ChorusError
+	if errors.As(err, &cErr) {
+		if cErr.CausedBy != nil {
+			logger.TechLog.Error(ctx, fmt.Sprintf("chorus error: %s, caused by: %v", cErr.ChorusCode, cErr.CausedBy))
+		}
+		return nil, cErr.ToGRPCStatus().Err()
 	}
 
+	// If it's already a gRPC status error, return it as is
 	if _, ok := status.FromError(err); ok {
 		return nil, err // Already a gRPC error
 	}
 
-	return nil, choruserrors.NewInternalError("An unexpected error occurred", err).ToGRPCStatus().Err()
+	return nil, cerr.ErrInternal.Wrap(err, "An unexpected error occurred.").ToGRPCStatus().Err()
 }
