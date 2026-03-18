6bb062fe6041b906fc68a3edde464a40dffb983a
Sami <sami.perrin@hotmail.com>
2026-03-18 14:28:50 +0100

    fix migrations

--- internal/migration/chorus/postgres/00035_user_email.sql
+++ internal/migration/chorus/postgres/00035_user_email.sql
@@ -1,6 +1,3 @@
 -- +migrate Up
 ALTER TABLE users ADD COLUMN email TEXT NOT NULL DEFAULT '';
 UPDATE users SET email = username WHERE username ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$';
-
--- +migrate Down
-ALTER TABLE users DROP COLUMN email;
