6a6bb318a13b855ccea953bb9969be4b250780a6
Jérémie Frei <jeremie.frei@chuv.ch>
2026-05-26 10:39:06 +0200

    fix: use correct app instances table for migration

--- internal/migration/chorus/postgres/00039_app_browser_config.sql
+++ internal/migration/chorus/postgres/00039_app_browser_config.sql
@@ -4,4 +4,4 @@ ALTER TABLE public.apps RENAME COLUMN kioskconfigurl TO browserconfigurl;
 ALTER TABLE public.apps RENAME COLUMN kioskconfigjwturl TO browserconfigjwturl;
 ALTER TABLE public.apps RENAME COLUMN kioskconfigjwtoidcclientid TO browserconfigjwtoidcclientid;
 
-ALTER TABLE public.appinstances RENAME COLUMN kioskconfigjwttoken TO browserconfigjwttoken;
+ALTER TABLE public.app_instances RENAME COLUMN kioskconfigjwttoken TO browserconfigjwttoken;
