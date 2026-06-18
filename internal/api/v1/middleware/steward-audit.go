0464d42fdab7d1744a76c3d00b2bc613fdac5329
Jonathan Haab <jonathan.haab@chuv.ch>
2026-06-18 14:49:29 +0200

    add tenant id to log

--- internal/api/v1/middleware/steward-audit.go
+++ internal/api/v1/middleware/steward-audit.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"fmt"
-
 	"github.com/CHORUS-TRE/chorus-backend/internal/api/v1/chorus"
 	"github.com/CHORUS-TRE/chorus-backend/internal/audit"
 	"github.com/CHORUS-TRE/chorus-backend/pkg/audit/model"
@@ -40,7 +39,8 @@ func (c stewardControllerAudit) InitializeTenant(ctx context.Context, req *choru
 		)
 	} else {
 		opts = append(opts,
-			audit.WithDescription(fmt.Sprintf("Initialized tenant %q.", req.Name)),
+			audit.WithDetail("id", res.Result.Id),
+			audit.WithDescription(fmt.Sprintf("Initialized tenant %q with id %d.", req.Name, res.Result.Id)),
 		)
 	}
 
