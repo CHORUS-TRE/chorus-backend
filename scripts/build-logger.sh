f498d55a4fd38d12ce26998cb5488b8676eb85d3
Jérémie Frei <jeremie.frei@chuv.ch>
2026-07-17 17:54:24 +0200

    refactor(scripts): remove unused scripts

--- scripts/build-logger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-set -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-cd "$DIR"
-
-go build -o cmd/logger/logger cmd/logger/main.go
--- scripts/run_dev.sh
+++
@@ -1,1 +0,0 @@
-go run ./cmd/chorus/main.go start | go run ./cmd/logger/main.go
