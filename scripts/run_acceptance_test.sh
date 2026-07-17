f010710649084371176c44983b14bf81a3d122b1
Jérémie Frei <jeremie.frei@chuv.ch>
2026-07-17 16:27:12 +0200

    refactor(acceptance): use test suite helpers and use v2 junit reporter flag

--- scripts/run_acceptance_test.sh
+++ scripts/run_acceptance_test.sh
@@ -5,7 +5,7 @@ set -e
 export TEST_CONFIG_FILE="./../../../configs/ci_local/backend-config.yaml"
 
 if [ -n "$1" ]; then
-    go test -count=1 -p 1 --tags acceptance "./tests/acceptance/$1"
+    go test -count=1 -p 1 --tags acceptance "./tests/acceptance/$1" -args --ginkgo.junit-report=junit.xml
 else
-    go test -count=1 -p 1 --tags acceptance ./tests/acceptance/...
+    go test -count=1 -p 1 --tags acceptance ./tests/acceptance/... -args --ginkgo.junit-report=junit.xml
 fi
--- tests/acceptance/app/app_suite_test.go
+++ tests/acceptance/app/app_suite_test.go
@@ -6,30 +6,8 @@ import (
 	"testing"
 
 	"github.com/CHORUS-TRE/chorus-backend/tests/helpers"
-
-	. "github.com/onsi/ginkgo/v2"
-	"github.com/onsi/ginkgo/v2/reporters"
-	. "github.com/onsi/gomega"
 )
 
 func TestAppService(t *testing.T) {
-	RegisterFailHandler(Fail)
-	junitReporter := reporters.NewJUnitReporter("junit.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, "App Service Suite", []Reporter{junitReporter})
-}
-
-var _ = AfterSuite(func() {
-	// cleanTables() - defined in app_test.go
-})
-
-func Then(text string, body func()) bool {
-	return It("then "+text, body)
-}
-
-func ExpectAPIErr(err interface{}) Assertion {
-	return helpers.ExpectAPIError(err)
-}
-
-func Given(text string, body func()) bool {
-	return Context("given "+text, body)
+	helpers.RunSuite(t, "App Service Suite")
 }
