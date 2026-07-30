7839197950dd4df2ab6fdf2050c72e2bd7f5043d
Jonathan Haab <jonathan.haab@chuv.ch>
2026-07-30 16:30:11 +0200

    add openid validation

--- internal/utils/validation/validation.go
+++ internal/utils/validation/validation.go
@@ -2,9 +2,12 @@ package validation
 
 import (
 	"context"
+	"reflect"
 	"regexp"
+	"strings"
 	"time"
 
+	"github.com/CHORUS-TRE/chorus-backend/internal/config"
 	"github.com/CHORUS-TRE/chorus-backend/internal/logger"
 
 	val "github.com/go-playground/validator/v10"
@@ -52,6 +55,19 @@ func NewValidator() *val.Validate {
 		logger.TechLog.Fatal(context.Background(), "unable to register validator 'ltetomorrowutc'", zap.Error(err))
 	}
 
+	// Report the yaml tag name instead of the Go field name
+	// (e.g. "daemon.grpc.host" instead of "Daemon.GRPC.Host")
+	// Falls back to the Go field name for structs with no yaml tag.
+	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("yaml"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	v.RegisterStructValidation(validateOpenIDMode, config.Mode{})
+
 	return v
 }
 
@@ -69,4 +85,34 @@ func ltetomorrowutc(fl val.FieldLevel) bool {
 	t := fl.Field().Interface().(time.Time)
 	d := time.Now().UTC().Add(48 * time.Hour).Truncate(24 * time.Hour)
 	return t.Before(d) || t.Equal(d)
+}
+
+// validateOpenIDMode requires the OpenID fields an "openid"-type mode actually
+// depends on.
+// These can't be expressed as required_if tags on OpenID's own fields
+func validateOpenIDMode(sl val.StructLevel) {
+	mode := sl.Current().Interface().(config.Mode)
+	if mode.Type != "openid" || !mode.Enabled {
+		return
+	}
+
+	o := mode.OpenID
+	reportIfEmpty := func(value, field, structField string) {
+		if value == "" {
+			sl.ReportError(value, field, structField, "required_openid", "")
+		}
+	}
+
+	reportIfEmpty(o.ID, "openid.id", "OpenID.ID")
+	reportIfEmpty(o.ClientID, "openid.client_id", "OpenID.ClientID")
+	reportIfEmpty(o.ClientSecret.PlainText(), "openid.client_secret", "OpenID.ClientSecret")
+	reportIfEmpty(o.AuthorizeURL, "openid.authorize_url", "OpenID.AuthorizeURL")
+	reportIfEmpty(o.TokenURL, "openid.token_url", "OpenID.TokenURL")
+	reportIfEmpty(o.UserInfoURL, "openid.user_info_url", "OpenID.UserInfoURL")
+
+	if o.EnableFrontendRedirect {
+		reportIfEmpty(o.ChorusFrontendRedirectURL, "openid.chorus_frontend_redirect_url", "OpenID.ChorusFrontendRedirectURL")
+	} else {
+		reportIfEmpty(o.ChorusBackendHost, "openid.chorus_backend_host", "OpenID.ChorusBackendHost")
+	}
 }
